4be5933af731e42bbe603fc09cfb75875ee3b46d
Andreas Hiropedi <s2015345@ed.ac.uk>
2023-04-03 12:37:38 +0100

    Update page-cache.h

---
+++ page-cache.h
@@ -0,0 +1,39 @@
+/* SPDX-License-Identifier: MIT /
+#pragma once
+
+#include <infos/util/lock.h>
+#include <infos/util/string.h>
+#include <arch/x86/pio.h>
+#include <infos/kernel/kernel.h>
+#include <infos/drivers/ata/ata-device.h>
+#include <infos/drivers/ata/ata-controller.h>
+#include <infos/drivers/block/block-device.h>
+
+namespace infos
+{
+    namespace drivers
+    {
+        namespace ata
+        {
+            struct Block {
+                    size_t id;
+                    uint8_t contents;
+                    Block next_block;
+                    Blockprev_block;
+                };
+            class LRUCache {
+                public:
+                    void init();
+
+                    bool read(void* buffer, size_t offset);
+
+                    void put(void* contents, size_t offset);
+
+                private:
+                    Block first = nullptr;
+                    Blocklast = nullptr;
+                    uint16_t size= 0;
+            };
+        }
+    }
+}
